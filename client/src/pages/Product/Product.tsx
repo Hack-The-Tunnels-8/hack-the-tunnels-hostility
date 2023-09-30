11b65c6f3197be719b92cb170dfba36eb74b6ed3
Allan <ALLANCAO@cmail.carleton.ca>
2023-09-30 14:05:02 -0400

    "Style Product Page Columns"

--- client/src/pages/Product/Product.tsx
+++ client/src/pages/Product/Product.tsx
@@ -30,12 +30,20 @@ function Product() {
         {product && (
           <>
             <div className="product-page__product">
-              <h3>Title: {product.title}</h3>
-              <p>ID: {id}</p>
-              <p>Description: {product.description}</p>
+              <div style={{display: "flex"}}>
+                <div>
+                <img src="https://www.ikea.com/ca/en/images/products/mammut-childrens-chair-indoor-outdoor-red__0727924_pe735940_s5.jpg" width="300" height="400"></img>
+                </div>
+                <div>
+                  <h3>Title: {product.title}</h3>
+                  <p>ID: {id}</p>
+                  <p>Description: {product.description}</p>
+                </div>
+              </div>
             </div>
             <Link to={`/checkout/${product.id}`}>
               <button>Buy Now</button>
+              
             </Link>
           </>
         )}
